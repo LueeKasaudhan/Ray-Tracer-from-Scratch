51980c9b14c495a7400973785fb11c43e2589417
lueekasaudhan <lueekasaudhan@gmail.com>
2025-12-01 02:49:21 +0530

    Updated vec3.h

--- vec3.h
+++ vec3.h
@@ -8,7 +8,7 @@ class vec3 {
   public:
     double e[3];
     vec3(): e{0,0,0} {} 
-    vec3(double x, double y, double z): e{x,y,z} {} 
+    vec3(double e0, double e1, double e2): e{e0, e1, e2} {} 
 
     double x() const { return e[0]; }
     double y() const { return e[1]; }
@@ -93,3 +93,57 @@ class vec3 {
 
 using point3 = vec3;
 //point3 is an alias for vec3
+
+// inline utility function
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+  return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+// avoid copying the std::ostream object and to allow modification of the stream.
+// const keyword means the function won't modify the vec3 object
+// Uses:- vec3 v;
+//        std::cout << v;
+
+
+inline vec3& operator+(const vec3& u, const vec3& v) {
+  return vec3(
+      u.e[0] + v.e[0]
+      u.e[1] + v.e[1]
+      u.e[2] + v.e[2]
+      );
+}
+inline vec3& operator-(const vec3& u, const vec3& v) {
+  return vec3(
+      u.e[0] - v.e[0]
+      u.e[1] - v.e[1]
+      u.e[2] - v.e[2]
+      );
+}
+inline vec3& operator*(const vec3& u, const vec3& v) {
+  return vec3(
+      u.e[0] * v.e[0]
+      u.e[1] * v.e[1]
+      u.e[2] * v.e[2]
+      );
+}
+inline vec3& operator*(double t, const vec3& v) {
+  return vec3(
+      t * v.e[0]
+      t * v.e[1]
+      t * v.e[2]
+      );
+}
+inline vec3& operator*(const vec3& v, double t) {
+  return t * v;
+}
+inline vec3& operator/(const vec3& u, double t) {
+  return (1/t) * v;
+}
+inline double dot(const vec3& u, const vec3& v) {
+  return 
+    u.e[0] * v.e[0] +
+    u.e[1] * v.e[1] +
+    u.e[2] * v.e[2] ;
+}
+inline vec3 cross(const vec3& u, const vec3& v) {}
+inline vec3 unit_vector(const vec3& u, const vec3& v) {}
